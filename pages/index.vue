0ea1414cb3772eae2da2304de106a5534befc52d
DamienForafo <damien.forafo@gmail.com>
2022-04-25 17:26:11 +0200

    axios test

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+const axios = require('axios').default;
 export default {
   data() {
     return {
@@ -13,6 +14,18 @@ export default {
     const id = "666633333";
     console.log(this.$auth);
     try {
+
+      // Axios test
+      axios({
+        method: 'get',
+        url: 'http://localhost:3000/products',
+        headers: {
+          'Content-Type': 'application/json',
+          'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU0NDNiYTM5ZWJjYzA0M2EwN2UzMDgiLCJpYXQiOjE2NDk2ODk1MzV9.P0t6d8gWp0ze8Xn1KZ_CDx_TFJzEL37LtGlSqmT9mQQ'
+        }
+      }).then(function (res) {console.log(res);});
+      // End of Axios test
+
       fetch(`http://localhost:3000/users/${id}`, {
         headers: {
           "Content-Type": "application/json",
